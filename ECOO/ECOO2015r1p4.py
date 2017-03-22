70f28b5a55b7983f05c6c72e20a40c677a80b540
marshmelllo <robfarlow@gmail.com>
2017-03-22 16:19:12 -0400

    ecoo2015r1p4.py

---
+++ ECOO/ECOO2015r1p4.py
@@ -0,0 +1,22 @@
+for _ in range(10):
+    n = input()
+    p = set(["ook","ookook","oog","ooga","ug","mook","mookmook","oogam","oogum","ugug"])
+    v = {}
+    def find(x):    
+        if x in v.keys():
+            return v[x]
+        
+        if x == "":
+            return 1
+        
+        
+        tot = 0
+        for i in range(2,len(x)+1):        
+            if x[:i] in p:
+                aa = x[i:]
+                a = find(aa)
+                v[aa] = a
+                tot += a
+        return tot
+    
+    print(find(n))
