9575d8f3c897bc4c6c01efef4459331de2f862e7
marshmelllo <robfarlow@gmail.com>
2017-04-14 01:13:31 -0400

    Back To School Dodgeball

---
+++ DMOJ/BacktoSchoolDodgeball.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+names = input().split()
+v = [1]
+for i in range(1,n):
+    s = names[i][0]
+    if s == names[i-1][0]:
+        v[-1] += 1
+    else:
+        v.append(1)
+print(v)
+t = 0
+for y in v:
+    t += int(y*(y+1)/2)
+
+print(t%1000000007)
+
