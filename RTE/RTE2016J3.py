bc013fc297d88be4e8009b84b9e29050b0c39fb4
Rob Farlow <robfarlow@gmail.com>
2017-06-08 10:03:50 -0400

    Create RTE2016J3.py

---
+++ RTE/RTE2016J3.py
@@ -0,0 +1,22 @@
+t = int(input())
+for _ in range(t):
+    n,c = map(int,input().split())
+    beans = [0 for i in range(n+1)]
+    donezo = [False for i in range(n+1)]
+    for i in range(c):
+        inp = input().split()
+        if inp[0] == "A":
+            a,y,x = inp; y = int(y); x = int(x);
+            if not donezo[x]:
+                beans[x]+=y
+        else:
+            a,x = inp; x = int(x)
+            if a == "Q":
+                print(beans[x])
+            elif a == "G":
+                print(sum(beans[:x+1]))
+            elif a == "L":
+                print(sum(beans[n-x+1:]))
+            else:
+                beans[x] = 0
+                donezo[x] = True
