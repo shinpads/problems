34103ad8e6472337de4a7e4ddc206afa5a9f3145
marshmelllo <robfarlow@gmail.com>
2017-11-11 14:20:44 -0500

    CCC2016S5

---
+++ CCC/CCC2016S5.py
@@ -0,0 +1,12 @@
+n,t = map(int,input().split())
+a = list(map(int,list(input())))
+atemp = [0 for i in range(n)]
+for i in range(50,-1,-1):
+    if((t>>i)&1):
+        p = (1<<i)%n
+        p2 = (n-p)%n
+        for x in range(0,n):
+            atemp[x] = a[(x+p)%n]^a[(x+p2)%n]
+        for x in range(n):
+            a[x] = atemp[x]
+print(''.join(list(map(str,a))))
